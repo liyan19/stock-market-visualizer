3081f10bcfd88fa86e64e839b29f68fe44ce6fb1
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-01-21 15:22:28 -0500

    Update NbcStockMarketApplication.java

--- src/main/java/com/conuhacks/nbc/NbcStockMarketApplication.java
+++ src/main/java/com/conuhacks/nbc/NbcStockMarketApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NbcStockMarketApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Holla world");
 		SpringApplication.run(NbcStockMarketApplication.class, args);
 	}
 
