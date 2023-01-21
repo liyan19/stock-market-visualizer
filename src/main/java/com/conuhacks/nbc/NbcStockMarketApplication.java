0ea81db613b13cc47439aaffeb84fa8d5dc485e7
walidoow <walidoo_2002@hotmail.com>
2023-01-21 15:02:52 -0500

    Initial commit

---
+++ src/main/java/com/conuhacks/nbc/NbcStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.conuhacks.nbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcStockMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbcStockMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conuhacks/nbc/NbcStockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conuhacks.nbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcStockMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
